e257747e6817c4f97eca1e3432834bde5a44eebb
reabel <ryanreabel@gmail.com>
2023-05-25 15:53:25 -0400

    updating with actual content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        Reabelx.com
+      </header>
+      <body>
+        <a href="https://blog.reabelx.com" 
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Blog
         </a>
-      </header>
+         | 
+        <a href="https://resume-reabel.vercel.app/" 
+          className="App-link"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          Resume
+        </a>
+      </body>
+      <footer  className="App-footer">
+        Copyright 2023 Reabelx
+      </footer>
     </div>
   );
 }
